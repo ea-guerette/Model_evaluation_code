8736c5b9da903c3a75c245bf37efa19e3ca4db39
ea-guerette <eag873@uowmail.edu.au>
2017-03-28 17:59:54 +1100

    Continued work on Figures_met_paper.R and tested it using BOM and WRF-ANSTO results - plots will need some cosmetic work but code is getting there for hourly values. Will need to add time averaging and more stats, and maybe export stats to csv. Also, density function for wind direction.

--- Figures_met_paper.R
+++ Figures_met_paper.R
@@ -3,54 +3,70 @@ library(plyr)
 library(lattice)
 library(latticeExtra)
 
-target_list <- c("temp", "RH", "ws", "u10", "v10", "prcp")
-target_names <- c("temperature", "RH (%)", "wind speed (m/s)", "u wind", "v wind", "precip")
-date_start <- c("21/12/2012","01/02/2011", "01/04/2012")
-date_end <- c("15/02/2013","07/03/2011","17/05/2012")
+#load in data 
+setwd("C:/Documents and Settings/eag873/My Documents/R_Model_Intercomparison/Campaign data")
+load("BOM_data_updated.RData")
+setwd("C:/Documents and Settings/eag873/My Documents/R_Model_Intercomparison/Model output/")
+load("ANSTO_model_output.RData")
 
-for (i in 1:length(target_list)) {
+#assign variables
+BOM <- bom_data_all_campaigns
+models <- wrf
+site_list <- levels(as.factor(BOM$site))
+species_list <- c("temp", "RH", "ws", "u10", "v10", "prcp")
+species_names <- c("temperature", "RH (%)", "wind speed (m/s)", "u wind", "v wind", "precip")
+date_start <- c("21/12/2012","01/02/2011", "01/04/2012") #check proper dates
+date_end <- c("15/02/2013","07/03/2011","17/05/2012") #check proper dates 
+stat_list <- c("r", "RMSE", "MB")
 
-  d <- timeVariation(wrf, pollutant = target_list[i], group = "data_source", type = "campaign", ci = T, ylab = target_names[i])
+#select sites 
+model_met <- subset(models, site %in% site_list)
+#create site info 
+site_lat <- as.numeric(levels(as.factor(model_met$site_lat))) 
+site_lon <- as.numeric(levels(as.factor(model_met$site_lon)))
+site <- site_list
+site_info <- data.frame(site, site_lat, site_lon)
+
+#merge obs and model output into wide format 
+met <- merge(BOM, model_met, by = c("date", "site", "campaign"), suffixes = c(".obs", ".mod"), all = TRUE)
+#merge obs and model output into long format 
+BOM$data_source <- "OBS"
+met_ln <- rbind.fill(BOM, model_met)
+
+#plot diurnal cycles and time series for all species in species_list 
+for (i in 1:length(species_list)) {
+
+  d <- timeVariation(met_ln, pollutant = species_list[i], group = "data_source", type = "campaign", ci = T, ylab = species_names[i], strip.)
   print(d, subset = "hour")
   
   for (j in 1:length(date_start)){
-  scatterPlot(selectByDate(wrf, start = date_start[j], end = date_end[j]), x = "date", y = target_list[i], ylab = target_names[i], group = "data_source", type = "campaign", plot.type = "l")
+  scatterPlot(selectByDate(met_ln, start = date_start[j], end = date_end[j]), x = "date", y = species_list[i], ylab = species_names[i], group = "data_source", type = "campaign", plot.type = "l")
   }
 }
-#the above will work for showing all models, averaged across all sites separately for each campaign 
-#will need to reduce the number of sites to include only the BOM sites! 
- 
+#the above shows all models, averaged across all sites, separately for each campaign 
 
 
-#this will require wide data... 
-TaylorDiagram(sps2, obs = "WS.obs", mod = "WS.mod", group = "data_source.mod")
-stats <- modStats(sps2, obs = "WS.obs",mod = "WS.mod", type = "data_source.mod")
-
-#what about spatial analysis? - need to compute stats first - wide format 
-
-means <- ddply(wrf, .(site,campaign, data_source), numcolwise(mean), na.rm = TRUE)
-
-#strip = function(...) strip.default(style=1, ...) 
-#strip.left = function(...) strip.custom(style = 1, horizontal = F, strip.left = T)
-
+#plot Taylor diagrams and compute stats for all species in species_list and plot google bubble plots
 strip = function(...) strip.default(...)
 strip.left = strip.custom(style=1, horizontal = F)
-a1 <- GoogleMapsPlot(means, latitude = "site_lat", longitude = "site_lon", pollutant = "ws",
-                     maptype = "roadmap", col = "jet", cex = 2, main = "mean wind speed", 
-                     key.footer = "(m/s)", xlab = "lon", ylab = "lat", type = c( "campaign", "data_source"), strip.left = my.strip.left)
-#this does not work - function is broken (cannot find function "strip")
-useOuterStrips(a1$plot)
+
+for (k in 1:length(species_list)){
+TaylorDiagram(met, obs = paste0(species_list[k],".obs"), mod = paste0(species_list[k],".mod"), group = "data_source", type = "campaign", main = paste0(species_names[k]))
+
+  stats <- modStats(met, obs = paste0(species_list[k],".obs"), mod = paste0(species_list[k],".mod"), type = c("data_source","site", "campaign"))
+  #merge stats with site info... (lost when applying modStats)
+  stats <- merge(stats, site_info, by = "site")
+  
+    for (m in 1:length(stat_list)) {
+    a1 <- GoogleMapsPlot(stats, latitude = "site_lat", longitude = "site_lon", pollutant = stat_list[m],
+                         maptype = "roadmap", col = "jet", cex = 2, main = paste(stat_list[m], "-",species_list[k]  ),
+                         key.footer = "", xlab = "lon", ylab = "lat", type = c( "campaign", "data_source"))
+  useOuterStrips(a1$plot)
+}
+  #rename dataframe
+  stats_name <- paste0("stats_",species_list[k])
+  assign(stats_name,stats)
+}
 
 
-
-GoogleMapsPlot(stats_ws_sps1_sites, latitude = "site_lat", longitude = "site_lon", pollutant = "r", type = "data_source.mod",
-               maptype = "roadmap", col = "jet", cex = 3, main = "SPS1 - correlation - Wind speed")
-
-
-
-
-
-#so how do I loop through these plots? 
-mod_list <- levels(as.factor(wrf$data_source))
-stat_list <- c("r", "RMSE", "MB")
 
